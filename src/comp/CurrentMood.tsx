d0270c4c6b46043dab0d31115ddaaf033dccd513
Shneor <mm4471661@gmail.com>
2025-08-27 12:41:57 +0300

    basic 0

---
+++ src/comp/CurrentMood.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function CurrentMood() {
+  return (
+    <>
+      <div className="mood">
+        <h1>Mood Selector</h1>
+        <h2>What is my mood now?</h2>
+        <div className="CurrentMood">
+          😐
+          <p>Neutral</p>
+        </div>
+      </div>
+    </>
+  );
+}
